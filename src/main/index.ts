22e9c41324625a44ced30a4aa39b480171bd4318
Sanjay Govind <govindsanj@ecs.vuw.ac.nz>
2019-06-18 20:39:54 +1200

    Update version

--- src/main/index.ts
+++ src/main/index.ts
@@ -16,7 +16,7 @@ function createMainWindow() {
   const window = new BrowserWindow();
 
   if (isDevelopment) {
-    window.webContents.openDevTools();
+    // window.webContents.openDevTools();
   }
   if (isDevelopment) {
     window.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`);
