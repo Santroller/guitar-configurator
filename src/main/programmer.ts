6668c0e607d39a14ec12d9062d2703f2bb933087
Sanjay Govind <sanjay.govind9@gmail.com>
2019-07-07 18:55:10 +1200

    Instead of swapping to a specific driver on windows, just remove the winusb driver and refresh

--- src/main/programmer.ts
+++ src/main/programmer.ts
@@ -8,7 +8,7 @@ import { DeviceType, EepromConfig, Guitar, Subtype } from '../common/avr-types';
 import { readData, EepromConfigLength, writeData, defaultConfig } from './eeprom';
 import { searchForProgrammer } from './programmerFirmware';
 import { controlTransfer } from './programmerUtils';
-import { swapToWinUSB } from './programmerWindows';
+import { swapToWinUSB, restoreController } from './programmerWindows';
 // import * as xinput from "xinput";
 usbDetection.startMonitoring();
 
@@ -34,40 +34,41 @@ export async function searchForGuitar(cb: (guitar: Guitar) => void) {
   let found = false;
   let init = true;
   let devCb = async function (device: usb.Device) {
-    usbDetection.find(device.deviceDescriptor.idVendor, device.deviceDescriptor.idProduct, (err, dev)=>{
-      console.log(dev);
-      console.log(device);
-      //TODO there is no easy way to tell if we are referring to the same device.
-      //TODO can we write our own version of either usbDetection or node-usb that has all this on one?
-      //TODO Also, we only need to do this for controllers, we can just do a seperate list collection for serial devices.
-    });
+    // usbDetection.find(device.deviceDescriptor.idVendor, device.deviceDescriptor.idProduct, (err, dev) => {
+    //   console.log(dev);
+    //   console.log(device);
+    //   //TODO there is no easy way to tell if we are referring to the same device.
+    //   //TODO can we write our own version of either usbDetection or node-usb that has all this on one?
+    //   //TODO Also, we only need to do this for controllers, we can just do a seperate list collection for serial devices.
+    // });
     if (device.deviceDescriptor.idVendor == VID &&
       device.deviceDescriptor.idProduct == PID) {
       usb.removeListener('attach', devCb);
       found = true;
-      if (process.platform == 'win32') {
-        console.log(device);
-        //For xinput, we have the ability to work out what kind of device we are using.
-        let found = false;
-        [0, 1, 2, 3].forEach(id => {
-          if (xinput.IsConnected(id)) {
-            let config = defaultConfig;
-            config.subtype = xinput.GetCapabilities(id).subType;
-            cb({ type: DeviceType.Unknown, config, updating: false });
-            found = true;
-          }
-        })
-        if (!found) {
-          let config = defaultConfig;
-          config.subtype = Subtype.Gamepad;
-          cb({ type: DeviceType.Unknown, config, updating: false });
-        }
-      } else {
-        await swapToWinUSB();
-        let config = await readConfig();
-        let type = detectType(config);
-        cb({ type, config, updating: false });
-      }
+      // if (process.platform == 'win32') {
+      //   console.log(device);
+      //   //For xinput, we have the ability to work out what kind of device we are using.
+      //   let found = false;
+      //   [0, 1, 2, 3].forEach(id => {
+      //     if (xinput.IsConnected(id)) {
+      //       let config = defaultConfig;
+      //       config.subtype = xinput.GetCapabilities(id).subType;
+      //       cb({ type: DeviceType.Unknown, config, updating: false });
+      //       found = true;
+      //     }
+      //   })
+      //   if (!found) {
+      //     let config = defaultConfig;
+      //     config.subtype = Subtype.Gamepad;
+      //     cb({ type: DeviceType.Unknown, config, updating: false });
+      //   }
+      // } else {
+        await restoreController();
+      // await swapToWinUSB();
+      // let config = await readConfig();
+      // let type = detectType(config);
+      // cb({ type, config, updating: false });
+      // }
     } else if (!init) {
       setTimeout(async () => {
         let unprogrammed = await searchForProgrammer();
--- src/main/programmerWindows.ts
+++ src/main/programmerWindows.ts
@@ -5,23 +5,26 @@ import * as usb from 'usb';
 import * as fs from 'fs';
 import * as path from 'path';
 import { VID, PID } from './programmer';
-import { Guitar, OutputType } from '../common/avr-types';
 import { BinaryExecution, executeBinary } from './programmerUtils';
+import { IPty } from 'node-pty';
 let infDir = tmp.dirSync();
 
 /**
  * Change drivers for all connected controllers
  */
-async function runDevCon(command: string, inf?: string): BinaryExecution {
+async function runDevCon(command: string, args: string[], targetDevice: boolean, onData?: (chunk: string, proc: IPty) => void): BinaryExecution {
     if (process.platform != 'win32') return { code: 0, msg: "" };
+    args = [command, ...args];
+    if (targetDevice) {
+        args.push(`USB\\VID_${VID.toString(16)}&PID_${PID.toString(16)}`);
+    } else {
+        return await executeBinary('devcon', args, onData);
+    }
     return await new Promise((resolve) => {
         let cb = async function (device: usb.Device) {
             if (device.deviceDescriptor.idVendor == VID && device.deviceDescriptor.idProduct == PID) {
                 usb.removeListener('attach', cb);
-                let args = [command];
-                if (command == 'update') args.push(inf);
-                args.push(`USB\\VID_${VID.toString(16)}&PID_${PID.toString(16)}`);
-                resolve(await executeBinary('devcon', args));
+                resolve(await executeBinary('devcon', args, onData));
             }
         }
         let dev = usb.findByIds(VID, PID);
@@ -42,10 +45,9 @@ export async function swapToWinUSB() {
         //Call zadic
         await executeBinary('zadic', ['--vid', `${VID}`, '--pid', `${PID}`, '--usealldevices', '--noprompt'], () => { }, infDir.name);
     }
-    await runDevCon('disable');
-    //Run devcon so it can change the drivers to the generated one
-    //Return code 1 means requires reboot / needs replug
-    if ((await runDevCon('update', inf)).code == 1) {
+    // Run devcon so it can change the drivers to the generated one
+    // Return code 1 means requires reboot / needs replug
+    if ((await runDevCon('update', [inf], true)).code == 1) {
         //TODO: in this case, tell the user to unplug and replug.
         console.log("Unplug, replug.");
         await new Promise((resolve) => {
@@ -64,13 +66,20 @@ export async function swapToWinUSB() {
             usb.on('detach', cb);
         });
     }
-    await runDevCon('enable');
 }
-export async function restoreController(guitar: Guitar) {
+export async function restoreController() {
     if (process.platform != 'win32') return;
-    if (guitar.config.output_type == OutputType.XInput) {
-        await runDevCon('update', 'c:\\Windows\\INF\\xusb22.inf');
-    } else {
-        await runDevCon('update', 'c:\\Windows\\INF\\input.inf');
+    let inf;
+    let isWinUsb = false;
+    await runDevCon('driverfiles', [], true, chunk => {
+        if (chunk.includes('inf')) {
+            inf = chunk.split('INF\\')[1].split(' ')[0].trim();
+        }
+        isWinUsb = isWinUsb || chunk.includes('winusb');
+    })
+    if (inf && isWinUsb) {
+        await runDevCon('remove', [], true, c => console.log(c));
+        await runDevCon('dp_delete', [inf], false, c => console.log(c));
+        await runDevCon('rescan', [], false, c => console.log(c));
     }
 }
