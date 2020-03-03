d38515ceaab7cca992dc431735457413345b9bc0
Sanjay Govind <sanjay.govind9@gmail.com>
2020-03-03 20:32:43 +1300

    Update Ardwiino

--- update-ardwiino.sh
+++ update-ardwiino.sh
@@ -4,6 +4,7 @@ git fetch
 git reset --hard origin/master
 git submodule init
 git submodule pull --remote
+git pull --tags
 git describe --abbrev=0 --tags > output/version
 make build-all
 cp -rf output/* ../../firmware/
