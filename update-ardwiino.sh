cbab87841916c8739ec6b224c5a4fdba208cf56c
Sanjay Govind <sanjay.govind9@gmail.com>
2020-04-26 19:55:28 +1200

    Update Ardwiino

--- update-ardwiino.sh
+++ update-ardwiino.sh
@@ -6,6 +6,12 @@ git submodule init
 git submodule update
 git describe --abbrev=0 --tags > output/version
 make clean
+rm -r src/uno/usb/obj
+rm -r src/uno/main/obj
+rm -r src/micro/obj
+rm -r src/uno/usb/bin
+rm -r src/uno/main/bin
+rm -r src/micro/bin
 make build-all
 cp -rf output/* ../../firmware/
 cd ../../
