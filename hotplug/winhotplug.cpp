f51182b81ac9a69ae9dc8f5bdb2c3ff4e75ae950
Sanjay Govind <sanjay.govind9@gmail.com>
2020-09-22 22:12:52 +1200

    Fix windows actually

--- hotplug/winhotplug.cpp
+++ hotplug/winhotplug.cpp
@@ -3,29 +3,28 @@
 
 #include <QDebug>
 
-#pragma comment(lib, "user32.lib" )
-#pragma comment(lib, "hid.lib" )
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "hid.lib")
 #pragma comment(lib, "setupapi.lib")
 #pragma comment(lib, "Cfgmgr32.lib")
 #pragma comment(lib, "OneCoreUAP.lib")
 
-#include <Windows.h>
 #include <Cfgmgr32.h>
 #include <Dbt.h>
+#include <Windows.h>
 #include <setupapi.h>
 
 #undef DEFINE_GUID
-#define DEFINE_GUID(name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
-    static const GUID name = { l,w1,w2,{ b1,b2,b3,b4,b5,b6,b7,b8 } }
-DEFINE_GUID( GUID_DEVINTERFACE_HID, 0x4D1E55B2L, 0xF16F, 0x11CF, 0x88, 0xCB, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30);
-#include <Usbiodef.h>
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+    static const GUID name = {l, w1, w2, {b1, b2, b3, b4, b5, b6, b7, b8}}
+DEFINE_GUID(GUID_DEVINTERFACE_HID, 0x4D1E55B2L, 0xF16F, 0x11CF, 0x88, 0xCB, 0x00, 0x11, 0x11, 0x00, 0x00, 0x30);
 #include <Usbioctl.h>
+#include <Usbiodef.h>
+
 #include <QRegularExpression>
 
-bool operator== (const QSerialPortInfo &lhs, const QSerialPortInfo &rhs)
-{
-    return lhs.manufacturer() == rhs.manufacturer()
-            && lhs.serialNumber() == rhs.serialNumber();
+bool operator==(const QSerialPortInfo& lhs, const QSerialPortInfo& rhs) {
+    return lhs.manufacturer() == rhs.manufacturer() && lhs.serialNumber() == rhs.serialNumber();
 }
 
 QList<QString> buses;
@@ -38,19 +37,19 @@ void lookupUSBInfo(bool isHID, wchar_t* dbcc_name, HWND hwnd, UsbDevice_t* devic
     infoData.cbSize = sizeof(infoData);
     if (SetupDiOpenDeviceInterfaceW(handle, dbcc_name, 0, &data)) {
         int memberIndex = 0;
-        while(SetupDiEnumDeviceInfo(handle, memberIndex, &infoData)) {
+        while (SetupDiEnumDeviceInfo(handle, memberIndex, &infoData)) {
             memberIndex++;
             if (isHID) {
                 device->hidPath = QString::fromWCharArray(dbcc_name);
                 wchar_t* ifaceList;
+                CONFIGRET cres;
                 while (true) {
-                    cres = CM_Get_Parent(&infoData.DevInst,infoData.DevInst,NULL);
+                    cres = CM_Get_Parent(&infoData.DevInst, infoData.DevInst, NULL);
                     if (cres != CR_SUCCESS) {
                         break;
                     }
                     CM_Get_Device_IDW(infoData.DevInst, szDeviceInstanceID, MAX_DEVICE_ID_LEN, 0);
                     // Get list size
-                    CONFIGRET cres;
                     ULONG ifaceListSize = 0;
                     GUID dev = GUID_DEVINTERFACE_USB_DEVICE;
                     cres = CM_Get_Device_Interface_List_SizeW(&ifaceListSize, &dev, szDeviceInstanceID, CM_GET_DEVICE_INTERFACE_LIST_PRESENT | CM_GET_DEVICE_INTERFACE_LIST_ALL_DEVICES);
@@ -62,7 +61,7 @@ void lookupUSBInfo(bool isHID, wchar_t* dbcc_name, HWND hwnd, UsbDevice_t* devic
                     ifaceList = new wchar_t[ifaceListSize];
 
                     // Populate the list
-                    cres = CM_Get_Device_Interface_ListW(&dev, szDeviceInstanceID, ifaceList, ifaceListSize, CM_GET_DEVICE_INTERFACE_LIST_PRESENT  | CM_GET_DEVICE_INTERFACE_LIST_ALL_DEVICES);
+                    cres = CM_Get_Device_Interface_ListW(&dev, szDeviceInstanceID, ifaceList, ifaceListSize, CM_GET_DEVICE_INTERFACE_LIST_PRESENT | CM_GET_DEVICE_INTERFACE_LIST_ALL_DEVICES);
                     if (cres != CR_SUCCESS) {
                         delete[] ifaceList;
                         qDebug() << "ERROR -13";
@@ -83,14 +82,14 @@ void lookupUSBInfo(bool isHID, wchar_t* dbcc_name, HWND hwnd, UsbDevice_t* devic
                 SetupDiGetDeviceRegistryPropertyA(handle, &infoData, SPDRP_HARDWAREID, NULL, (PBYTE)&hardwareID, sizeof(hardwareID), &size);
                 QRegularExpression idMatch(".+VID_(\\w{4})&PID_(\\w{4}).+");
                 auto match = idMatch.globalMatch(QString::fromUtf8(hardwareID)).next();
-                device->vid = match.captured(1).toInt(NULL,16);
-                device->pid = match.captured(2).toInt(NULL,16);
+                device->vid = match.captured(1).toInt(NULL, 16);
+                device->pid = match.captured(2).toInt(NULL, 16);
                 device->port = port_nr;
                 CM_Get_Device_IDW(infoData.DevInst, szDeviceInstanceID, MAX_DEVICE_ID_LEN, 0);
                 qDebug() << QString::fromWCharArray(szDeviceInstanceID);
                 device->serial = QString::fromWCharArray(szDeviceInstanceID).split("\\")[2];
-                while(infoData.DevInst) {
-                    CM_Get_Parent(&infoData.DevInst,infoData.DevInst,NULL);
+                while (infoData.DevInst) {
+                    CM_Get_Parent(&infoData.DevInst, infoData.DevInst, NULL);
                     CM_Get_Device_IDW(infoData.DevInst, szDeviceInstanceID, MAX_DEVICE_ID_LEN, 0);
                     int bus_number = buses.indexOf(QString::fromWCharArray(szDeviceInstanceID));
                     if (bus_number != -1) {
@@ -104,8 +103,8 @@ void lookupUSBInfo(bool isHID, wchar_t* dbcc_name, HWND hwnd, UsbDevice_t* devic
     }
     SetupDiDestroyDeviceInfoList(handle);
 }
-WinHotplug::WinHotplug(PortScanner* scanner):scanner(scanner) {
-    for (auto a:QSerialPortInfo::availablePorts()) {
+WinHotplug::WinHotplug(PortScanner* scanner) : scanner(scanner) {
+    for (auto a : QSerialPortInfo::availablePorts()) {
         m_port_list.push_back(a);
         scanner->serialDeviceDetected(a);
     }
@@ -119,7 +118,7 @@ WinHotplug::WinHotplug(PortScanner* scanner):scanner(scanner) {
     //First, we need to loop trough all usb host controllers and assign them an id in order, as this is how libusb gets its bus number
     //Doing this allows us to support detecting multiple unos in dfu mode at once.
     for (;;) {
-        if (!SetupDiEnumDeviceInfo(handle,_index, &infoData)) {
+        if (!SetupDiEnumDeviceInfo(handle, _index, &infoData)) {
             if (GetLastError() != ERROR_NO_MORE_ITEMS) {
                 return;
             }
@@ -143,7 +142,7 @@ WinHotplug::WinHotplug(PortScanner* scanner):scanner(scanner) {
     }
     _index = 0;
     for (;;) {
-        if (!SetupDiEnumDeviceInfo(handle,_index, &infoData)) {
+        if (!SetupDiEnumDeviceInfo(handle, _index, &infoData)) {
             if (GetLastError() != ERROR_NO_MORE_ITEMS) {
                 SetupDiDeleteDeviceInterfaceData(handle, &data);
                 break;
@@ -160,13 +159,12 @@ WinHotplug::WinHotplug(PortScanner* scanner):scanner(scanner) {
                     continue;
                 }
             } else {
-
                 qDebug() << "expected insuff";
             }
             size *= sizeof(wchar_t);
             PSP_DEVICE_INTERFACE_DETAIL_DATA_W dev_interface_details = (PSP_DEVICE_INTERFACE_DETAIL_DATA_W)malloc(size);
-            dev_interface_details->cbSize =  sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA_W);
-            if (!SetupDiGetDeviceInterfaceDetailW(handle, &data,  dev_interface_details, size, NULL, NULL)) {
+            dev_interface_details->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA_W);
+            if (!SetupDiGetDeviceInterfaceDetailW(handle, &data, dev_interface_details, size, NULL, NULL)) {
                 DWORD lastError = GetLastError();
                 qDebug() << "unable to get path" << lastError;
                 continue;
@@ -182,57 +180,49 @@ WinHotplug::WinHotplug(PortScanner* scanner):scanner(scanner) {
         // Device does not have an interface matching this GUID, skip
     }
     SetupDiDestroyDeviceInfoList(handle);
-
 }
-bool WinHotplug::nativeEventFilter(const QByteArray &eventType, void *message, long *result)
-{
+bool WinHotplug::nativeEventFilter(const QByteArray& eventType, void* message, long* result) {
     if (eventType == "windows_generic_MSG") {
         MSG* msg = reinterpret_cast<MSG*>(message);
         auto msgType = msg->message;
-        if(msgType == WM_DEVICECHANGE)
-        {
+        if (msgType == WM_DEVICECHANGE) {
             PDEV_BROADCAST_HDR lpdb = reinterpret_cast<PDEV_BROADCAST_HDR>(msg->lParam);
-            switch(msg->wParam)
-            {
-            case DBT_DEVICEARRIVAL:
-                if (lpdb -> dbch_devicetype == DBT_DEVTYP_PORT)
-                {
-                    PDEV_BROADCAST_PORT_W lpdbv = reinterpret_cast<PDEV_BROADCAST_PORT_W>(lpdb);
-                    for (auto p: QSerialPortInfo::availablePorts()) {
-                        if (p.portName().contains(QString::fromWCharArray(lpdbv->dbcp_name))) {
-                            m_port_list.push_back(p);
-                            scanner->serialDeviceDetected(p);
-                            return false;
+            switch (msg->wParam) {
+                case DBT_DEVICEARRIVAL:
+                    if (lpdb->dbch_devicetype == DBT_DEVTYP_PORT) {
+                        PDEV_BROADCAST_PORT_W lpdbv = reinterpret_cast<PDEV_BROADCAST_PORT_W>(lpdb);
+                        for (auto p : QSerialPortInfo::availablePorts()) {
+                            if (p.portName().contains(QString::fromWCharArray(lpdbv->dbcp_name))) {
+                                m_port_list.push_back(p);
+                                scanner->serialDeviceDetected(p);
+                                return false;
+                            }
                         }
+                    } else if (lpdb->dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE) {
+                        PDEV_BROADCAST_DEVICEINTERFACE_W lpdbv = reinterpret_cast<PDEV_BROADCAST_DEVICEINTERFACE_W>(lpdb);
+                        bool isHID = lpdbv->dbcc_classguid == GUID_DEVINTERFACE_HID;
+                        UsbDevice_t dev = {};
+                        lookupUSBInfo(isHID, lpdbv->dbcc_name, msg->hwnd, &dev);
+                        scanner->add(dev);
                     }
-                } else if(lpdb -> dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE) {
-                    PDEV_BROADCAST_DEVICEINTERFACE_W lpdbv = reinterpret_cast<PDEV_BROADCAST_DEVICEINTERFACE_W>(lpdb);
-                    bool isHID = lpdbv->dbcc_classguid == GUID_DEVINTERFACE_HID;
-                    UsbDevice_t dev = {};
-                    lookupUSBInfo(isHID, lpdbv->dbcc_name, msg->hwnd, &dev);
-                    scanner->add(dev);
-
-                }
-                break;
-            case DBT_DEVICEREMOVECOMPLETE:
-                if (lpdb -> dbch_devicetype == DBT_DEVTYP_PORT)
-                {
-                    PDEV_BROADCAST_PORT_W lpdbv = reinterpret_cast<PDEV_BROADCAST_PORT_W>(lpdb);
-                    for (auto p: m_port_list) {
-                        if (p.portName().contains(QString::fromWCharArray(lpdbv->dbcp_name))) {
-                            m_port_list.removeOne(p);
-                            scanner->serialDeviceUnplugged(p);
-                            return false;
+                    break;
+                case DBT_DEVICEREMOVECOMPLETE:
+                    if (lpdb->dbch_devicetype == DBT_DEVTYP_PORT) {
+                        PDEV_BROADCAST_PORT_W lpdbv = reinterpret_cast<PDEV_BROADCAST_PORT_W>(lpdb);
+                        for (auto p : m_port_list) {
+                            if (p.portName().contains(QString::fromWCharArray(lpdbv->dbcp_name))) {
+                                m_port_list.removeOne(p);
+                                scanner->serialDeviceUnplugged(p);
+                                return false;
+                            }
                         }
+                    } else if (lpdb->dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE) {
+                        PDEV_BROADCAST_DEVICEINTERFACE_W lpdbv = reinterpret_cast<PDEV_BROADCAST_DEVICEINTERFACE_W>(lpdb);
+                        bool isHID = lpdbv->dbcc_classguid == GUID_DEVINTERFACE_HID;
+                        UsbDevice_t dev = {};
+                        lookupUSBInfo(isHID, lpdbv->dbcc_name, msg->hwnd, &dev);
+                        scanner->remove(dev);
                     }
-                } else if(lpdb -> dbch_devicetype == DBT_DEVTYP_DEVICEINTERFACE) {
-                    PDEV_BROADCAST_DEVICEINTERFACE_W lpdbv = reinterpret_cast<PDEV_BROADCAST_DEVICEINTERFACE_W>(lpdb);
-                    bool isHID = lpdbv->dbcc_classguid == GUID_DEVINTERFACE_HID;
-                    UsbDevice_t dev = {};
-                    lookupUSBInfo(isHID, lpdbv->dbcc_name, msg->hwnd, &dev);
-                    scanner->remove(dev);
-                }
-
             }
         }
     }
@@ -240,27 +230,25 @@ bool WinHotplug::nativeEventFilter(const QByteArray &eventType, void *message, l
 }
 void WinHotplug::init(WId wid) {
     DEV_BROADCAST_DEVICEINTERFACE NotificationFilter;
-    ZeroMemory( &NotificationFilter, sizeof(NotificationFilter) );
+    ZeroMemory(&NotificationFilter, sizeof(NotificationFilter));
     NotificationFilter.dbcc_size = sizeof(DEV_BROADCAST_DEVICEINTERFACE);
     NotificationFilter.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
     NotificationFilter.dbcc_classguid = GUID_DEVINTERFACE_COMPORT;
 
     DEV_BROADCAST_DEVICEINTERFACE NotificationFilter2;
-    ZeroMemory( &NotificationFilter2, sizeof(NotificationFilter) );
+    ZeroMemory(&NotificationFilter2, sizeof(NotificationFilter));
     NotificationFilter2.dbcc_size = sizeof(DEV_BROADCAST_DEVICEINTERFACE);
     NotificationFilter2.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
     NotificationFilter2.dbcc_classguid = GUID_DEVINTERFACE_HID;
 
-
     DEV_BROADCAST_DEVICEINTERFACE NotificationFilter3;
-    ZeroMemory( &NotificationFilter3, sizeof(NotificationFilter) );
+    ZeroMemory(&NotificationFilter3, sizeof(NotificationFilter));
     NotificationFilter3.dbcc_size = sizeof(DEV_BROADCAST_DEVICEINTERFACE);
     NotificationFilter3.dbcc_devicetype = DBT_DEVTYP_DEVICEINTERFACE;
     NotificationFilter3.dbcc_classguid = GUID_DEVINTERFACE_USB_DEVICE;
 
-
-    RegisterDeviceNotification(reinterpret_cast<HWND>(wid),&NotificationFilter, DEVICE_NOTIFY_WINDOW_HANDLE );
-    RegisterDeviceNotification(reinterpret_cast<HWND>(wid),&NotificationFilter2, DEVICE_NOTIFY_WINDOW_HANDLE );
-    RegisterDeviceNotification(reinterpret_cast<HWND>(wid),&NotificationFilter3, DEVICE_NOTIFY_WINDOW_HANDLE );
+    RegisterDeviceNotification(reinterpret_cast<HWND>(wid), &NotificationFilter, DEVICE_NOTIFY_WINDOW_HANDLE);
+    RegisterDeviceNotification(reinterpret_cast<HWND>(wid), &NotificationFilter2, DEVICE_NOTIFY_WINDOW_HANDLE);
+    RegisterDeviceNotification(reinterpret_cast<HWND>(wid), &NotificationFilter3, DEVICE_NOTIFY_WINDOW_HANDLE);
 }
 #endif
