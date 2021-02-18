4e4e4e6a445ff0cc4b44b61d93feafab6e659da0
Sanjay Govind <sanjay.govind9@gmail.com>
2021-02-18 21:41:46 +1300

    Pull avrdude and picotool out of modules as we need to make changes to them

--- libraries/dfu-programmer/src/main.c
+++ libraries/dfu-programmer/src/main.c
@@ -24,7 +24,11 @@
 #include <stdio.h>
 #include <string.h>
 #ifdef HAVE_LIBUSB_1_0
+#ifdef __APPLE__
 #include <libusb.h>
+#else
+#include <libusb-1.0/libusb.h>
+#endif
 #else
 #include <usb.h>
 #endif
